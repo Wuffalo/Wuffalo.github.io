a5c1c12f6d484109ed7fd2ac54581131d18d9ddb
Wolf <waminskey@gmail.com>
2017-05-24 18:50:39 -0500

    Fix toggler code

--- theme/js/show_hide_toggler.js
+++ theme/js/show_hide_toggler.js
@@ -1,4 +1,4 @@
-(document).ready(function () {
+$(document).ready(function () {
    $("button.toggle-start-hidden").parent().next().hide();
    $("button.toggle-start-hidden").click(function(){ $(this).parent().next().toggle(); });
    $("button.toggle").click(function(){ $(this).parent().next().toggle(); });
